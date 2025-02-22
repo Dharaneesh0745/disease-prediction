e20f19c6e5273ddb0e9381322e91c51d19562e2d
Dharaneesh Boobalan <dharaneesh0745@gmail.com>
2025-02-22 19:40:04 +0530

    Add model training stage with configuration updates and training logic.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.kidneyDiseaseClassifier import logger
 from src.kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.kidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.kidneyDiseaseClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 # STAGE_NAME = "01 - Stage => Data Ingestion"
@@ -14,12 +15,24 @@ from src.kidneyDiseaseClassifier.pipeline.stage_02_prepare_base_model import Pre
 #     raise e
 
 
-STAGE_NAME = "02 - Stage => Prepare Base Model"
+# STAGE_NAME = "02 - Stage => Prepare Base Model"
+# try: 
+#    logger.info(f"*******************")
+#    logger.info(f">>>>>> {STAGE_NAME} started. <<<<<<")
+#    prepare_base_model = PrepareBaseModelTrainingPipeline()
+#    prepare_base_model.main()
+#    logger.info(f">>>>>> {STAGE_NAME} completed. <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+     
+     
+STAGE_NAME = "03 - Stage => Model Training"
 try: 
    logger.info(f"*******************")
    logger.info(f">>>>>> {STAGE_NAME} started. <<<<<<")
-   prepare_base_model = PrepareBaseModelTrainingPipeline()
-   prepare_base_model.main()
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> {STAGE_NAME} completed. <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
